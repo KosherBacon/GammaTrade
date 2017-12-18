050d74ca55700ad17d55c96c11b2d7cb3ccca8ae
Joshua Kahn <j.harris.kahn@gmail.com>
2017-12-17 22:30:34 -1000

    Refactor Trader.

--- src/main/java/gy/jk/tick/TickModule.java
+++ src/main/java/gy/jk/tick/TickModule.java
@@ -1,14 +1,16 @@
 package gy.jk.tick;
 
 import com.google.inject.AbstractModule;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import gy.jk.tick.Annotations.TickLengthMillis;
 
 public class TickModule extends AbstractModule {
 
-  private static final long TICK_LENGTH_MILLIS = 60 * 1000; // 60 seconds per tick
+  private static final Config TRADE_CONFIG = ConfigFactory.load("application.conf");
 
   @Override
   protected void configure() {
-    bindConstant().annotatedWith(TickLengthMillis.class).to(TICK_LENGTH_MILLIS);
+    bindConstant().annotatedWith(TickLengthMillis.class).to(TRADE_CONFIG.getLong("tickLength"));
   }
 }
