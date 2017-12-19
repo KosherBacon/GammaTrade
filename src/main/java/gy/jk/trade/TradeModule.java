6acad881582c35d08e0adeb4f1065fbafa28af9c
Joshua Kahn <j.harris.kahn@gmail.com>
2017-12-19 00:36:10 -1000

    Add different strategies to TradeModule.

--- src/main/java/gy/jk/trade/TradeModule.java
+++ src/main/java/gy/jk/trade/TradeModule.java
@@ -3,9 +3,7 @@ package gy.jk.trade;
 import com.google.inject.AbstractModule;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-import gy.jk.strategy.PrimaryStrategy;
-import gy.jk.strategy.RSIStrategy;
-import gy.jk.strategy.StrategyBuilder;
+import gy.jk.strategy.*;
 import gy.jk.trade.Annotations.MaximumOrderSize;
 import gy.jk.trade.Annotations.TradeStrategy;
 import org.knowm.xchange.currency.CurrencyPair;
@@ -28,7 +26,25 @@ public class TradeModule extends AbstractModule {
         bind(StrategyBuilder.class).annotatedWith(TradeStrategy.class).to(PrimaryStrategy.class);
         break;
       case "rsi":
-        bind(StrategyBuilder.class).annotatedWith(TradeStrategy.class).to(RSIStrategy.class);
+        bind(StrategyBuilder.class).to(RSIStrategy.class);
+        break;
+      case "globalExtrema":
+        bind(StrategyBuilder.class).to(GlobalExtremaStrategy.class);
+        break;
+      case "cciCorrection":
+        bind(StrategyBuilder.class).to(CCICorrectionStrategy.class);
+        break;
+      case "kurt":
+        bind(StrategyBuilder.class).to(KurtStrategy.class);
+        break;
+      case "momentum":
+        bind(StrategyBuilder.class).to(MomentumStrategy.class);
+        break;
+      case "bollinger":
+        bind(StrategyBuilder.class).to(BollingerStrategy.class);
+        break;
+      case "honeyBadger":
+        bind(StrategyBuilder.class).to(HoneyBadgerStrategy.class);
         break;
     }
     bind(BigDecimal.class).annotatedWith(MaximumOrderSize.class).toInstance(MAXIMUM_ORDER_SIZE);
