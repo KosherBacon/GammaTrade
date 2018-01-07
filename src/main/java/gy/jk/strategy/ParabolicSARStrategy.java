cadfbc63c65edaa124b9433fa622db2ae242a541
Joshua Kahn <j.harris.kahn@gmail.com>
2018-01-07 14:51:41 -0500

    Work on ParabolicSAR strategy.

--- src/main/java/gy/jk/strategy/ParabolicSARStrategy.java
+++ src/main/java/gy/jk/strategy/ParabolicSARStrategy.java
@@ -1,12 +1,13 @@
 package gy.jk.strategy;
 
 import com.google.inject.Inject;
-import org.ta4j.core.Decimal;
-import org.ta4j.core.Strategy;
-import org.ta4j.core.TimeSeries;
+import org.ta4j.core.*;
 import org.ta4j.core.indicators.ParabolicSarIndicator;
 import org.ta4j.core.indicators.helpers.MaxPriceIndicator;
 import org.ta4j.core.indicators.helpers.MinPriceIndicator;
+import org.ta4j.core.indicators.helpers.PreviousValueIndicator;
+import org.ta4j.core.trading.rules.OverIndicatorRule;
+import org.ta4j.core.trading.rules.UnderIndicatorRule;
 
 public class ParabolicSARStrategy implements StrategyBuilder {
 
@@ -23,6 +24,18 @@ public class ParabolicSARStrategy implements StrategyBuilder {
     ParabolicSarIndicator parabolicSar =
         new ParabolicSarIndicator(timeSeries, Decimal.valueOf("0.025"), Decimal.valueOf("0.050"));
 
-    return null;
+    PreviousValueIndicator second = new PreviousValueIndicator(parabolicSar);
+    PreviousValueIndicator third = new PreviousValueIndicator(second);
+
+    // Trend confirmed when all three indicate the same trend.
+    Rule entryRule = new UnderIndicatorRule(parabolicSar, minPrice)
+        .and(new UnderIndicatorRule(second, minPrice))
+        .and(new UnderIndicatorRule(third, minPrice));
+
+    Rule exitRule = new OverIndicatorRule(parabolicSar, maxPrice)
+        .and(new OverIndicatorRule(second, maxPrice))
+        .and(new OverIndicatorRule(third, maxPrice));
+
+    return new BaseStrategy(entryRule, exitRule);
   }
 }
